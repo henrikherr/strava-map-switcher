d79d74a3eb449cecdb18d1fa27577f7b59c0e606
Henrik Herr <55617275+henrikherr@users.noreply.github.com>
2025-07-16 12:38:47 +0200

    Update layers.js

--- layers.js
+++ layers.js
@@ -66,5 +66,9 @@ var AdditionalMapLayers;
 		geoportail: {name: "Geoportail Aerial [FR]",
 			url: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
 			opts: {maxZoom: 20, maxNativeZoom: 19, attribution: geoportailAttr}},
+		kompass: {name: "Kompass Touristic [DE/AT/IT]",
+			url: "https://map2.kompass.de/{z}/{x}/{y}/kompass_touristic?key=2ba8c124-38b6-11e7-ade1-e0cb4e14e399&proj=outdooractive",
+			opts: {minZoom: 5, maxZoom: 18, attribution: '© <a href=\"https://www.kompass.de/\">Kompass Karten</a>'}},
+
 	};
 }
